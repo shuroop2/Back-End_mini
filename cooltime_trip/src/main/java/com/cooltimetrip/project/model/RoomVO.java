16e2bde960d9c9eab133f1b8947a7796dd363c39
kyeogrok <kyeongrok@naver.com>
2022-07-21 09:26:41 +0900

    update java file

--- cooltime_trip/src/main/java/com/cooltimetrip/project/model/RoomVO.java
+++ cooltime_trip/src/main/java/com/cooltimetrip/project/model/RoomVO.java
@@ -1,48 +1,27 @@
 package com.cooltimetrip.project.model;
 
 public class RoomVO {
-	private String roomNo;
-	private String roomType;
-	private int roomPrice;
-	private String roomView;
+	
 	private String hotelNo;
-	private String hotelImg;
+	private String hotelName;
+	private String hotelGrade;
+	private String hotelAddress;
+	private String hotelAssesment;
+	private String hotelLocation;
+	private String hotelContact;
+	private String hotelRule;
+	private String hotelTime;
+	private String roomT;
+	private String roomD;
 	
-	public String getRoomNo() {
-		return roomNo;
-	}
-	public void setRoomNo(String roomNo) {
-		this.roomNo = roomNo;
-	}
-	public String getRoomType() {
-		return roomType;
-	}
-	public void setRoomType(String roomType) {
-		this.roomType = roomType;
-	}
-	public int getRoomPrice() {
-		return roomPrice;
-	}
-	public void setRoomPrice(int roomPrice) {
-		this.roomPrice = roomPrice;
-	}
-	public String getRoomView() {
-		return roomView;
-	}
-	public void setRoomView(String roomView) {
-		this.roomView = roomView;
-	}
-	public String getHotelNo() {
-		return hotelNo;
-	}
-	public void setHotelNo(String hotelNo) {
-		this.hotelNo = hotelNo;
-	}
-	public String getHotelImg() {
-		return hotelImg;
-	}
-	public void setHotelImg(String hotelImg) {
-		this.hotelImg = hotelImg;
-	}
 	
+	private String roomTType;
+	private int roomTPrice;
+	private String roomTView;
+	private String hotelTImg;
+	
+	private String roomDType;
+	private int roomDPrice;
+	private String roomDView;
+	private String hotelDImg;
 }
